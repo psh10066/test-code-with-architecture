b9b4d8af98780d1f05b26b7dffe9e1cd44977f9a
이호준 <psh10066@gmail.com>
2024-06-01 17:56:37 +0900

    [어쨌든 테스트 코드] 테스트 추가하기: mockmvc를 이용한 controller 테스트

---
+++ src/test/resources/sql/user-controller-test-data.sql
@@ -0,0 +1,4 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (1, 'psh10066@naver.com', 'psh10066', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'ACTIVE', 0);
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (2, 'hjlee@naver.com', 'hjlee', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab', 'PENDING', 0);
